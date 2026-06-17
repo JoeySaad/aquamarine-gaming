5f850052d319482174d4b6bf14d6f783d6197655
JoeySaad <saadjoey678@gmail.com>
2026-06-17 16:32:28 -0400

    Show redesigned site on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  redirect("/index.html");
+  return (
+    <iframe
+      src="/index.html"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "0",
+        display: "block",
+      }}
+      title="Aquamarine Gaming"
+    />
+  );
 }
